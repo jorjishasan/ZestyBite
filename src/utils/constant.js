8280e5c425c91fbc449b5a91371285fbe755cf96
jorjishasan <dev.jorjis@gmail.com>
2024-10-06 04:12:09 +0600

    add: store constant urls/api

---
+++ src/utils/constant.js
@@ -0,0 +1,10 @@
+const SWIGGY_API_URL =
+  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9716&lng=77.5946";
+
+const CATEGORY_API_URL =
+  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/";
+
+const RESTAURANT_CARD_IMAGE_URL =
+  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/";
+
+export { SWIGGY_API_URL, CATEGORY_API_URL, RESTAURANT_CARD_IMAGE_URL };
