af8f6a3aab401b852e9c6fd9ff3b8213d2bc2453
jorjishasan <dev.jorjis@gmail.com>
2024-10-06 04:13:14 +0600

    add: add font-family globally

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./components/Header";
+import Body from "./components/Body";
+function App() {
+  return (
+    <div className="font-sans">
+      <Header />
+      <Body />
+    </div>
+  );
+}
+
+export default App;
