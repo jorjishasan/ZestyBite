fcaed1a1dceabfa9e0c428c6a94d38939a1b072d
jorjishasan <dev.jorjis@gmail.com>
2024-10-06 04:16:58 +0600

    add(redux): Implement restaurant slice

---
+++ src/redux/restaurantsSlice.js
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit";
+import mockData from "../mocks/mockdata.json";
+
+const initialState = {
+  foodCategory: mockData?.data?.cards[0],
+  topRestaurants: mockData?.data?.cards[1],
+  restaurantChains: mockData?.data?.cards[2],
+};
+
+const restaurantsSlice = createSlice({
+  name: "restaurants",
+  initialState,
+  reducers: {
+    setRestaurantsData: (state, action) => {
+      state.foodCategory = action.payload.cards[0];
+      state.topRestaurants = action.payload.cards[1];
+      state.restaurantChains = action.payload.cards[2];
+    },
+  },
+});
+
+export const { setRestaurantsData } = restaurantsSlice.actions;
+export default restaurantsSlice.reducer;
