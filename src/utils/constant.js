3ce39f19bd2b5d1c0b34133338ce85225bbb8999
jorjishasan <dev.jorjis@gmail.com>
2024-10-10 04:29:55 +0600

    add: restaurant menu api url

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -7,4 +7,12 @@ const CATEGORY_API_URL =
 const RESTAURANT_CARD_IMAGE_URL =
   "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/";
 
-export { SWIGGY_API_URL, CATEGORY_API_URL, RESTAURANT_CARD_IMAGE_URL };
+const RESTAURANT_MENU_API_URL =
+  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9716&lng=77.5946&restaurantId=";
+
+export {
+  SWIGGY_API_URL,
+  CATEGORY_API_URL,
+  RESTAURANT_CARD_IMAGE_URL,
+  RESTAURANT_MENU_API_URL,
+};
