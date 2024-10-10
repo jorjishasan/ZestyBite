fd9236ada0a5521bed683f0cfcec1975b97cc999
jorjishasan <dev.jorjis@gmail.com>
2024-10-10 16:45:51 +0600

    add: update redux store accordingly

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import restaurantsReducer from "./restaurantsSlice";
+import restaurantMenuReducer from "./restaurantMenuSlice";
 
 export const store = configureStore({
   reducer: {
     restaurants: restaurantsReducer,
+    restaurantMenu: restaurantMenuReducer,
   },
 });
