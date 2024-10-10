d9993a7b7fe5053260b418d0823e63d339bcc0f3
jorjishasan <dev.jorjis@gmail.com>
2024-10-10 16:47:37 +0600

    add: Restaurant Menu component

--- src/components/RestaurantMenu.jsx
+++ src/components/RestaurantMenu.jsx
@@ -1,5 +1,32 @@
+import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+import useGetRestaurantMenu from "../hooks/useGetRestaurantMenu";
+
 const RestaurantMenu = () => {
-  return <div>RestaurantMenu</div>;
+  const { id } = useParams();
+  useGetRestaurantMenu(id);
+
+  const { info, itemCards, offers } = useSelector(
+    (state) => state.restaurantMenu,
+  );
+
+  if (!info || !itemCards) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <h1>{info.name}</h1>
+      {offers && offers.length > 0 && (
+        <div>
+          <h2>Offers</h2>
+          {/* Render offers */}
+        </div>
+      )}
+      <h2>Menu</h2>
+      {/* Render itemCards */}
+    </div>
+  );
 };
 
 export default RestaurantMenu;
