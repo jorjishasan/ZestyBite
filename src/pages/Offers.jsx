55ed4207834ccdb44ee261f0368d60d6556bbb92
jorjishasan <dev.jorjis@gmail.com>
2024-10-12 04:23:05 +0600

    add: offer page

--- src/pages/Offers.jsx
+++ src/pages/Offers.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/prop-types */
 
 import { FaTag, FaClock } from "react-icons/fa";
+import { offers } from "../utils/offersData";
 
 const OfferCard = ({ title, description, discount, expiryDate, imageUrl }) => (
   <div className="group relative overflow-hidden rounded-lg shadow-lg transition-all duration-300 hover:shadow-xl">
@@ -28,57 +29,6 @@ const OfferCard = ({ title, description, discount, expiryDate, imageUrl }) => (
 );
 
 const Offers = () => {
-  const offers = [
-    {
-      title: "Pizza Bonanza",
-      description: "Get amazing discounts on all pizzas",
-      discount: 30,
-      expiryDate: "2023-08-31",
-      imageUrl:
-        "https://images.unsplash.com/photo-1513104890138-7c749659a591?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
-    },
-    {
-      title: "Burger Feast",
-      description: "Huge savings on our signature burgers",
-      discount: 25,
-      expiryDate: "2023-09-15",
-      imageUrl:
-        "https://images.unsplash.com/photo-1568901346375-23c9450c58cd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1899&q=80",
-    },
-    {
-      title: "Sushi Delights",
-      description: "Premium sushi at unbeatable prices",
-      discount: 40,
-      expiryDate: "2023-09-30",
-      imageUrl:
-        "https://images.unsplash.com/photo-1579871494447-9811cf80d66c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
-    },
-    {
-      title: "Pasta Paradise",
-      description: "Indulge in our pasta dishes for less",
-      discount: 35,
-      expiryDate: "2023-10-15",
-      imageUrl:
-        "https://images.unsplash.com/photo-1551183053-bf91a1d81141?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1932&q=80",
-    },
-    {
-      title: "Healthy Bowls",
-      description: "Get fit with our discounted salad bowls",
-      discount: 20,
-      expiryDate: "2023-10-31",
-      imageUrl:
-        "https://images.unsplash.com/photo-1512621776951-a57141f2eefd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
-    },
-    {
-      title: "Dessert Fiesta",
-      description: "Treat yourself with discounted desserts",
-      discount: 45,
-      expiryDate: "2023-11-15",
-      imageUrl:
-        "https://images.unsplash.com/photo-1551024506-0bccd828d307?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1364&q=80",
-    },
-  ];
-
   return (
     <div className="bg-gray-100">
       {/* Hero Section */}
