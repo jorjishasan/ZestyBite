9e4ae19460bfc57e2444fb1a118fd663fd4107b1
jorjishasan <dev.jorjis@gmail.com>
2024-10-12 01:56:50 +0600

    add: create a new 'cart' slice

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import restaurantsReducer from "./restaurantsSlice";
 import restaurantMenuReducer from "./restaurantMenuSlice";
+import cartReducer from "./cartSlice";
 
 export const store = configureStore({
   reducer: {
     restaurants: restaurantsReducer,
     restaurantMenu: restaurantMenuReducer,
+    cart: cartReducer,
   },
 });
