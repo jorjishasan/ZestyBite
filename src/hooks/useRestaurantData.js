eb7419be193aa37855a55f8a3fe96fdc09be49b8
jorjishasan <dev.jorjis@gmail.com>
2024-10-25 03:08:38 +0600

    feat(memoization): check the restaurant data presents on redux store before pulling it from new api calls.

--- src/hooks/useRestaurantData.js
+++ src/hooks/useRestaurantData.js
@@ -1,15 +1,28 @@
 import { useState, useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { setRestaurantsData } from "../redux/restaurantsSlice";
-import { SWIGGY_API_URL } from "../utils/constant";
 import mockData from "../mocks/mockdata.json";
+import { SWIGGY_API_URL } from "../utils/constant";
 
 const useRestaurantData = () => {
   const dispatch = useDispatch();
   const [isLoading, setIsLoading] = useState(true);
 
+  // Access the current state of restaurants
+  const restaurants = useSelector((state) => state.restaurants);
+
   useEffect(() => {
     const fetchData = async () => {
+      // Check if data is already present
+      if (
+        restaurants.foodCategory &&
+        restaurants.topRestaurants &&
+        restaurants.restaurantChains
+      ) {
+        setIsLoading(false);
+        return;
+      }
+
       try {
         const response = await fetch(SWIGGY_API_URL);
         const json = await response.json();
@@ -23,7 +36,7 @@ const useRestaurantData = () => {
     };
 
     fetchData();
-  }, [dispatch]);
+  }, [dispatch, restaurants]);
 
   return { isLoading };
 };
