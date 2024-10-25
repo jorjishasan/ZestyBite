73caa66c0efcc8294d552519beee7ed4316977d1
jorjishasan <dev.jorjis@gmail.com>
2024-10-26 01:47:22 +0600

    chore: implement mobile first approach

--- src/components/ResMenuOfferCard.jsx
+++ src/components/ResMenuOfferCard.jsx
@@ -3,7 +3,7 @@ import { RESTAURANT_CARD_IMAGE_URL } from "../utils/constant";
 
 const ResMenuOfferCard = ({ offer }) => {
   return (
-    <div className="flex w-[400px] items-center justify-center gap-3 rounded-3xl border-2 border-gray-300 p-4">
+    <div className="flex w-52 flex-col items-center justify-center gap-3 rounded-3xl border-2 border-gray-300 p-4 md:w-[400px] md:flex-row">
       <div className="h-16 w-16">
         <img
           className="h-full w-full"
@@ -12,8 +12,12 @@ const ResMenuOfferCard = ({ offer }) => {
         />
       </div>
       <div>
-        <h3 className="text-nowrap text-lg font-bold">{offer?.info?.header}</h3>
-        <div className="truncate text-gray-400">{offer?.info?.description}</div>
+        <h3 className="text-nowrap text-sm font-bold md:text-lg">
+          {offer?.info?.header}
+        </h3>
+        <div className="truncate text-sm text-gray-400">
+          {offer?.info?.description}
+        </div>
       </div>
     </div>
   );
