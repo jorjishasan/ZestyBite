a84d33037b868fa2655fac032d99b16221724293
jorjishasan <dev.jorjis@gmail.com>
2024-10-26 01:28:53 +0600

    chore: change icons for better UX

--- src/components/ScrollButtons.jsx
+++ src/components/ScrollButtons.jsx
@@ -1,4 +1,6 @@
 /* eslint-disable react/prop-types */
+import rightArrow from "../assets/arrow-right.svg";
+import leftArrow from "../assets/arrow-left.svg";
 const ScrollButtons = ({
   onScrollLeft,
   onScrollRight,
@@ -11,18 +13,18 @@ const ScrollButtons = ({
       disabled={!canScrollLeft}
       className={`h-10 w-10 rounded-full text-center ${
         canScrollLeft ? "bg-slate-200" : "bg-slate-100"
-      } p-2`}
+      } p-3`}
     >
-      ←
+      <img src={leftArrow} alt="left arrow" />
     </button>
     <button
       onClick={onScrollRight}
       disabled={!canScrollRight}
       className={`h-10 w-10 rounded-full text-center ${
         canScrollRight ? "bg-slate-200" : "bg-slate-100"
-      } p-2`}
+      } p-3`}
     >
-      →
+      <img src={rightArrow} alt="right arrow" />
     </button>
   </span>
 );
