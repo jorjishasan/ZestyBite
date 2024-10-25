43097a40859e039651eb5ad5d5057357dafcfbc9
jorjishasan <dev.jorjis@gmail.com>
2024-10-25 06:03:14 +0600

    add: make responsiveness

--- src/components/PlatformPromotion.jsx
+++ src/components/PlatformPromotion.jsx
@@ -3,13 +3,13 @@ import playstore from "../assets/playstore.png";
 
 const PlatformPromotion = () => {
   return (
-    <div className="flex w-full flex-col items-center justify-center gap-8 bg-[#F0F0F5] py-10">
-      <h2 className="text-3xl font-semibold">
+    <div className="flex w-full flex-col items-center justify-center gap-4 bg-[#F0F0F5] py-10 md:gap-8">
+      <h2 className="text-md font-semibold sm:text-3xl">
         For better experience, Download the app now.
       </h2>
       <div className="flex gap-2">
-        <img className="h-[64px]" src={appstore} alt="appstore" />
-        <img className="h-[64px]" src={playstore} alt="playstore" />
+        <img className="h-[32px] md:h-[64px]" src={appstore} alt="appstore" />
+        <img className="h-[32px] md:h-[64px]" src={playstore} alt="playstore" />
       </div>
     </div>
   );
