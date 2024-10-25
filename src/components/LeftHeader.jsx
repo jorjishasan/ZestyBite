13b1a77fc1aa3a5adad839ed3b6e02d82906f34b
jorjishasan <dev.jorjis@gmail.com>
2024-10-26 01:52:30 +0600

    chore: update navigation link

--- src/components/LeftHeader.jsx
+++ src/components/LeftHeader.jsx
@@ -5,11 +5,13 @@ import locationIcon from "../assets/location-pin.svg";
 const LeftHeader = () => {
   return (
     <div className="flex items-center">
-      <img
-        src={brandIcon}
-        alt="Brand Logo"
-        className="sm:w-13 sm:h-13 h-10 w-10"
-      />
+      <Link to="/">
+        <img
+          src={brandIcon}
+          alt="Brand Logo"
+          className="sm:w-13 sm:h-13 h-10 w-10"
+        />
+      </Link>
       <div className="invisible ml-2 flex items-center text-gray-600 sm:visible sm:ml-4">
         <img
           src={locationIcon}
@@ -18,7 +20,7 @@ const LeftHeader = () => {
         />
         <span className="text-xs sm:text-sm">Rajshahi</span>
         <Link
-          to="/change-location"
+          to="/"
           className="ml-1 text-xs text-orange-500 underline sm:text-sm"
         >
           Change Location
